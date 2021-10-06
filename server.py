434a7717bbb69e43ca823cc827b38b841c4ac83c
xjq <xjq314@gmail.com>
2021-10-06 21:34:38 +0800

    remove settings

--- server.py
+++ server.py
@@ -19,6 +19,4 @@ def reset():
 
 if __name__ == '__main__':
     app.debug = True
-    app.jinjia_env.auto_reload = True
-    app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.run(host='0.0.0.0', port=80)
