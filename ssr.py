1d29e57e41d335b9c25c0fce03c16563b972232a
xjq <xjq314@gmail.com>
2021-11-21 12:59:30 +0800

    Create ssr.py

---
+++ ssr.py
@@ -0,0 +1,16 @@
+class SSR
+
+def __init__():
+    self.config_file = '/opt/shadowsocks/shadowsocks.json'
+    self.config = dict()
+    self.status = 'unknown'
+
+
+def commit():
+    pass
+
+def reset():
+    pass
+
+def _status_check():
+    pass
