91635b8473568b85f2ff82c5b462a0ad1b45616e
xjq <xjq314@gmail.com>
2022-05-02 17:40:43 +0000

    Update dbss.py

--- dbss.py
+++ dbss.py
@@ -4,13 +4,20 @@ import os
 instance_id = 'i-0e01cf02fba13bba5'
 hostedzone_id = 'Z0919409KSY4PU0EQGIP'
 
-a = os.system('rd /s/q C:\\Users\\xjq\\AppData\\Local\\Google\\Chrome')
-print a
+clear_command = "rd /s/q C:\\Users\\xjq\\AppData\\Local\\Google\\Chrome"
+print os.system(clear_command)
 print 'Create new Chrome environment ... done'
 
 print 'Get new IP address ...'
 new_ip = aws.change_eip(instance_id,hostedzone_id)
 print 'Your current IP is ' + new_ip
 
+create_command = "reg add \"HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings\" /v ProxyEnable /t REG_DWORD /d 1 /f"
+change_command = "reg add \"HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings\" /v ProxyServer /d \"" + new_ip + ":10086\" /f"
+
+print os.system(create_command)
+print os.system(change_command)
+
 print 'NOTICE! You will loose this IP forever after logout'
+
 
