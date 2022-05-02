24cc4169e4d49d2633614ee9d7fd22a77f2a88d5
xjq <xjq314@gmail.com>
2022-05-02 16:41:47 +0000

    dd dbss

---
+++ dbss.bat
@@ -0,0 +1,2 @@
+c:\python27\python.exe c:\users\xjq\desktop\smith\dbss.py
+pause
--- dbss.py
+++ dbss.py
@@ -1,7 +1,16 @@
 import aws
+import os
 
 instance_id = 'i-0e01cf02fba13bba5'
 hostedzone_id = 'Z0919409KSY4PU0EQGIP'
 
-print 'Cleanup Chrome history ...'
-print 'Cleanup Chrome history ... done'
+a = os.system('rd /s/q C:\\Users\\xjq\\AppData\\Local\\Google\\Chrome')
+print a
+print 'Create new Chrome environment ... done'
+
+print 'Get new IP address ...'
+new_ip = aws.change_eip(instance_id,hostedzone_id)
+print 'Your current IP is ' + new_ip
+
+print 'NOTICE! You will loose this IP forever after logout'
+
