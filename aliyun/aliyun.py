98da7f9f8ac1e1cdfbd9bc1d3e93232ffe7ba683
xjq <xjq314@gmail.com>
2022-06-26 22:33:06 +0800

    Create aliyun.py

---
+++ aliyun/aliyun.py
@@ -0,0 +1,27 @@
+import json
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    pass
+    
+# 1.Get current IP
+
+# 2.Remove IP from NAT Gateway
+
+# 3.Release old IP
+
+# 4.Allocate new IP
+
+# 5.Attach new IP to NAT Gateway
+
+# 6.Create DNAT rule
+
+# 7.Create SNAT rule
+
+
+
